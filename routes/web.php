10ea6e7e486b3ff09ee910b6d7a292681a22f41c
heyaji <gresare@ssct.edu.ph>
2025-08-27 08:36:56 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+
+Route::get('/', [AuthController::class, 'login'])->name('login');
+
+
+//Route::get('/', function () {
+//    return Inertia::render('Welcome');
+//})->name('home');
+//
+//
+//Route::get('dashboard', function () {
+//    return Inertia::render('Dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
+//
+//require __DIR__.'/settings.php';
+//require __DIR__.'/auth.php';
