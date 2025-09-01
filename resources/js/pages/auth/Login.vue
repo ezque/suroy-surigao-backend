9a9889d33196e3cffcca0d0f3cf7edb49e5e3a4e
heyaji <gresare@ssct.edu.ph>
2025-09-01 14:18:20 +0800

    done login design

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -1,23 +1,158 @@
 <template>
     <div class="login-container">
-        <div class="left-container">
-            <h2>Login hasdhs</h2>
-        </div>
-        <div class="right-container">
+        <div class="login-card">
+            <div class="card-left">
+                <div class="form-logo">
+                    <img src="/public/images/auth-logo.png" alt="logo" />
+                </div>
+                <div class="login-form-con" v-if="activeForm === 'login'">
+                    <h2>login</h2>
+                    <label>Email</label>
+                    <input placeholder="Email"/>
+                    <label>Password</label>
+                    <input placeholder="Password"/>
+                    <a>Forgot Password</a>
+                    <button>Sign in</button>
+                    <p>Don't Have an Account yet? <a  @click="activeForm = 'register'">register</a></p>
+                </div>
+                <div class="register-form-con" v-else>
+                    <a @click="activeForm = 'login'"> login</a>
+                </div>
 
+
+            </div>
+            <div class="card-right">
+                <img src="/public/images/logo/logo.png" alt="logo" />
+                <h2>welcome!</h2>
+                <h3>your journey starts here</h3>
+            </div>
         </div>
     </div>
 </template>
 <script setup lang="ts">
-    //
+    import { ref } from 'vue';
+
+    const activeForm = ref<'login' | 'register'>('login')
 </script>
 <style scoped>
     .login-container {
         width: 100%;
         height: 100vh;
         overflow: hidden;
-        background: linear-gradient(to bottom, #6ABBE4, #EEF8F9);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-image: url("/images/ilovesurigao.jpg");
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
     }
+    .login-card {
+        width: 60%;
+        height: 70%;
+        border-radius: 60px;
+        display: flex;
+        flex-direction: row;
+        border: 9px solid #A6D7EE;
+    }
+    .card-right {
+        width: 50%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+    .card-right img {
+        width: 50%;
+        height: auto;
+        margin: 0;
+    }
+    .card-right h2 {
+        margin: 0;
+        color: white;
+        text-transform: uppercase;
+        font-weight: bold;
+        font-size: 2em;
+    }
+    .card-right h3 {
+        color: white;
+        padding: 10px 40px 10px 40px;
+        background-color: #5DB1DB;
+        border-radius: 30px;
+        text-transform: capitalize;
+    }
+    .card-left {
+        width: 50%;
+        height: 100%;
+        border-top-left-radius: 40px;
+        border-bottom-left-radius: 40px;
+        background-color: #A6D7EE;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    .form-logo {
+        width: 100%;
+        height: 40%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .form-logo img {
+        width: 40%;
+        height: auto;
+    }
+    .login-form-con {
+        width: 70%;
+        height: 60%;
+        display: flex;
+        flex-direction: column;
+    }
+    .login-form-con h2 {
+        text-transform: capitalize;
+        color: #333333;
+        font-size: 1.4em;
+        margin: 0;
+        font-weight: bold;
+    }
+    .login-form-con label {
+        margin-top: 10px;
+        color: #333333;
+        font-size: 1em;
+    }
+    .login-form-con input {
+        width: 100%;
+        background-color: white;
+        padding: 10px;
+        border-radius: 10px;
+        margin-top: 5px;
+    }
+    .login-form-con a {
+        text-align: right;
+        font-size: 1.1em;
+        color: #F24822;
+        margin-top: 10px;
+        cursor: pointer;
+    }
+    .login-form-con button {
+        width: 100%;
+        padding: 5px;
+        background-color: #007A8C;
+        border-radius: 20px;
+        color: white;
+        font-weight: bold;
+        margin-top: 10px;
+    }
+    .login-form-con p {
+        text-align: center;
+        margin-top: 5px;
+        text-transform: capitalize;
+    }
+    .login-form-con p a {
+        cursor: pointer;
+    }
+
 </style>
 
 
